5e51e56cb80770d48d140a3010f6748234259c93
Night Lore <nathanlui2010@gmail.com>
2020-01-16 14:59:05 -0800

    Initial game objects

---
+++ StrandedOutcast/Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public int numSticks = 0;
+    public int numRocks = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Item"))
+        {
+            if (other.gameObject.name.Contains("Stick"))
+            {
+                numSticks++;
+                Destroy(other.gameObject);
+            }
+            else if (other.gameObject.name.Contains("Rock"))
+            {
+                numRocks++;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ StrandedOutcast/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10;
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+    }
+}
