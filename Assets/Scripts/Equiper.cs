50d0e031f9bc675775ba09460e810ff5126ea077
Michael Bauknecht <mbauknecht650@gmail.com>
2020-02-08 11:52:50 -0800

    Update script Equiper, added comments to functions

--- Assets/Scripts/Equiper.cs
+++ Assets/Scripts/Equiper.cs
@@ -6,13 +6,25 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Inventory))]
 public class Equiper : MonoBehaviour
 {
+    /*
+     * variables for scripts, references assigned in start()
+     */
     private Attacker attacker;
     private Inventory inventory;
+
 
     public GameObject[] weapons;
     public Sprite[] weaponImages;
     public Sprite defaultImage;
+
+    /*
+     * references to scripts
+     */
     private int currentWeapon = GameSettings.FISTS;
+
+    /*
+     * Objects with references assigned in start()
+     */
     private TextMeshProUGUI currentWeaponText;
     private Image equipImage;
 
@@ -26,6 +38,23 @@ public class Equiper : MonoBehaviour
         Equip(currentWeapon);
     }
 
+    /* Equip
+     * 
+     * Equips a weapon in the players hand, sets proper attack: damage,speed, size
+     *      -will not equip if in middle of an attack
+     *      -will not equip if weapon is not craftable
+     *      -crafts weapon when selected and possible to craft
+     * 
+     * References:
+     *      Scripts:    
+     *          attacker
+     *          GameSettings
+     *          Inventory
+     *          
+     *      GameOjects: 
+     *          currentWeaponText
+     *          equipImage
+     */
     public void Equip(int weapon)
     {
         // if attacking, don't equip
@@ -56,7 +85,20 @@ public class Equiper : MonoBehaviour
         currentWeapon = weapon; // update currentWeapon to new weapon
     }
 
-    // Attempt to craft weapon. Return true if successful.
+    /* CraftWeapon
+     * 
+     * Attempt to craft weapon. Return true if successful.
+     *      -Checks current inventory counts against saved 'recipe' counts
+     *      -If current inventory is enough item is crafted:
+     *          -counts in inventory is reduced by 'recipe' counts
+     *      -UI is updated for inventory quantity
+     *      
+     * References:
+     *      Scripts:
+     *          GameSettings
+     *          inventory
+     * 
+     */
     private bool CraftWeapon(int weapon)
     {
         int[] recipe = GameSettings.weaponParts[weapon];
