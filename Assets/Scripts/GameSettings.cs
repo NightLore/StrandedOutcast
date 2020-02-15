f24837efe36e1ccf352b0164def0ace111c2ce97
Stephanie <stephcarpin710@gmail.com>
2020-02-15 14:21:40 -0800

    Terrain changes

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameSettings : MonoBehaviour
 {
-    public static float bounds = 50.0f;
+    public static float bounds = 70.0f;
     public static int decorAmount = 200;
     public static int waterAmount = 50;
 
