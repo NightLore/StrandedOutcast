eee26c2abfb65a1c520f5e5db883ebb0c5a650b6
Night Lore <nathanlui2010@gmail.com>
2020-02-28 09:45:40 -0800

    Fix errors after dying

--- Assets/Scripts/NavAgent.cs
+++ Assets/Scripts/NavAgent.cs
@@ -24,7 +24,7 @@ public class NavAgent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (agent.pathPending) return;
+        if (agent.pathPending || !target) return;
 
         if (GameSettings.day)
         {
