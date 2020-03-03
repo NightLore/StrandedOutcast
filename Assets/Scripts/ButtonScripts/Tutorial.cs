193c893b1713987e8389f8f07d662cde9ba8609b
Clay Froelich <cxfroeli@calpoly.edu>
2020-03-02 16:42:39 -0800

    adapted tutorial to wait for input....except clicking on UI. That was too much trouble for me to figure out. Maybe extra onClick listeners that are turned off usually and only on for the tutorial section?

--- Assets/Scripts/ButtonScripts/Tutorial.cs
+++ Assets/Scripts/ButtonScripts/Tutorial.cs
@@ -15,12 +15,13 @@ public class Tutorial : MonoBehaviour
     }
     private void OnEnable()
     {
-        Invoke("ActivateMovementText", GameSettings.tutorialDelay * 0);
-        Invoke("DeactivateMovementText", GameSettings.tutorialDelay * 1);
-        Invoke("ActivateInteractText", GameSettings.tutorialDelay * 2);
-        Invoke("DeactivateInteractText", GameSettings.tutorialDelay * 3);
-        Invoke("ActivateCraftingText", GameSettings.tutorialDelay * 4);
-        Invoke("DeactivateCraftingText", GameSettings.tutorialDelay * 6);
+        StartCoroutine("WaitForMovement");
+        // Invoke("ActivateMovementText", GameSettings.tutorialDelay * 0);
+        // Invoke("DeactivateMovementText", GameSettings.tutorialDelay * 1);
+        // Invoke("ActivateInteractText", GameSettings.tutorialDelay * 2);
+        // Invoke("DeactivateInteractText", GameSettings.tutorialDelay * 3);
+        // Invoke("ActivateCraftingText", GameSettings.tutorialDelay * 4);
+        // Invoke("DeactivateCraftingText", GameSettings.tutorialDelay * 6);
     }
 
     // Update is called once per frame
@@ -57,5 +58,34 @@ public class Tutorial : MonoBehaviour
     private void DeactivateCraftingText()
     {
         craftingText.SetActive(false);
+    }
+
+    IEnumerator WaitForMovement()
+    {
+        ActivateMovementText();
+        while (!Input.GetKeyDown(KeyCode.W) && !Input.GetKeyDown(KeyCode.A) &&
+               !Input.GetKeyDown(KeyCode.S) && !Input.GetKeyDown(KeyCode.D)) {
+            yield return null;
+        }
+        DeactivateMovementText();
+        StartCoroutine("WaitForInteraction");
+    }
+
+    IEnumerator WaitForInteraction()
+    {
+        ActivateInteractText();
+        while (!Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+        DeactivateInteractText();
+        StartCoroutine("WaitForClick");
+    }
+
+    IEnumerator WaitForClick()
+    {
+        ActivateCraftingText();
+        
+        yield return new WaitForSeconds(GameSettings.tutorialDelay);
+        DeactivateCraftingText();
     }
 }
